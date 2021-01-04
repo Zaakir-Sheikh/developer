49da44765b284f0947d2ef2ee45b70db2bb0f5b7
ZSmaster1 <zaakirsheikh630@gmail.com>
2021-01-04 14:10:53 -0500

    added emailing

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,14 +2,19 @@ function sendMsg() {
     let Name = document.getElementById("msgName").value;
     let Email = document.getElementById("msgEmail").value;
     let phoneNumber = document.getElementById("msgPhoneNumber").value;
-    let message = document.getElementById("MsgMsg").value;
+    let Message = document.getElementById("MsgMsg").value;
     if(!Name){
         Swal.fire('Oops...', 'Please enter a name!', 'error');
     } else if(!Email){
         Swal.fire('Oops...', 'Please enter an email', 'error')
-    } else if(!message){
+    } else if(!Message){
         Swal.fire('Oops...', 'Please enter a message!', 'error')
     } else {
-        
+        let db = firebase.firestore();
+        db.collection('Emails').doc(Name).set({
+            Name: Name,
+            Email: Email,
+            Message: Message,
+        })
     }
 }
