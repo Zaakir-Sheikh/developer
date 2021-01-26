668e8fb16e2076d5fe93b8d6f2cbafcd3eb9edab
ZSmaster1 <zaakirsheikh630@gmail.com>
2021-01-26 17:09:37 -0500

    Create login.js

---
+++ apps/Assalaam Institute Login Client/js/login.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".sign-in-form");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    login();
+})
+
+function login() {
+    console.log(`test`)
+}
