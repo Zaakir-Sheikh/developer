351290eaaa7e163fe19bd6f2d4b6f4edafff87b9
ZSmaster1 <zaakirsheikh630@gmail.com>
2021-01-26 17:26:06 -0500

    Update login.js

--- apps/Assalaam Institute Login Client/js/login.js
+++ apps/Assalaam Institute Login Client/js/login.js
@@ -5,6 +5,64 @@ form.addEventListener('submit', (e) => {
     login();
 })
 
+function checkUser(UID, FILE) {
+    if(UID && FILE){
+      firebase.auth().onAuthStateChanged(function(user) {
+        if(user){
+          if(user.uid === UID) return window.location = FILE
+        } else {
+          let email = document.getElementById("email").value;
+          let password = document.getElementById("password").value;
+          firebase.auth().signInWithEmailAndPassword(email, password).then((user) => {
+            Swal.fire({
+              title: `${email} Logged in`,
+              timer: 10000,
+              timerProgressBar: true,
+              didOpen: () => {
+                timerInterval = setInterval(() => {
+                  const content = Swal.getContent()
+                  if (content) {
+                    const b = content.querySelector('b')
+                    if (b) {
+                      b.textContent = Swal.getTimerLeft()
+                    }
+                  }
+                }, 100)
+              },
+              willClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+                window.location = FILE
+            })
+          }).catch((error) => {
+            Swal.fire({
+              title: error.message,
+              timer: 10000,
+              timerProgressBar: true,
+              didOpen: () => {
+                timerInterval = setInterval(() => {
+                  const content = Swal.getContent()
+                  if (content) {
+                    const b = content.querySelector('b')
+                    if (b) {
+                      b.textContent = Swal.getTimerLeft()
+                    }
+                  }
+                }, 100)
+              },
+              willClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+              return;
+            })
+          })
+        }
+      })
+    }
+  }
+
 function login() {
-    console.log(`test`)
+    
 }
