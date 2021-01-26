edaeff7f0e236cba18e4bd9f7645e2a3b0164e93
ZSmaster1 <zaakirsheikh630@gmail.com>
2021-01-26 14:47:38 -0500

    a

---
+++ apps/Assalaam Institute Login Client/js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAtlc0LJdIytb-1_uKx9wIoCatPjooWsxc",
+    authDomain: "assalaam-institute.firebaseapp.com",
+    projectId: "assalaam-institute",
+    storageBucket: "assalaam-institute.appspot.com",
+    messagingSenderId: "51725736269",
+    appId: "1:51725736269:web:d4a91d19883350ed5b3f04"
+};
+
+firebase.initializeApp(firebaseConfig);
+const perf = firebase.performance();
